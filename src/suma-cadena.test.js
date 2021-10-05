13f899b73b6bd4b94def2f8f02656072de868924
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-10-05 03:43:38 -0400

    - calcular suma de una cadena ignorando numeros que sean mayores a mil ok

--- src/suma-cadena.test.js
+++ src/suma-cadena.test.js
@@ -8,17 +8,22 @@ describe("sumar cadena ", () => {
   it("calcular suma de una cadena con un delimitador por defecto", () => {
     expect(calcularCadena("6;7;4", ";")).toEqual(17);
   });
+  it("calcular suma de una cadena con un delimitador por defecto", () => {
+    expect(calcularCadena("2, 1001", "")).toEqual(2);
+  });
 });
 
 function calcularCadena(cadena, delimitador) {
   let subCadena = "", suma = 0, baseNumerico = 10;
   let entero;
   for(var i = 0; i <= cadena.length; i++){
-    if(parseInt(cadena[i], baseNumerico)){
+    if(parseInt(cadena[i], baseNumerico) || cadena[i] == 0){
       subCadena = subCadena + cadena[i];
     }else{
-      entero = parseInt(subCadena, baseNumerico)
-      if(!delimitador || ((cadena[i] == delimitador || !cadena[i]) && entero)) suma = suma + entero;
+      entero = parseInt(subCadena, baseNumerico);
+      if(!delimitador || ((cadena[i] == delimitador || !cadena[i]) && entero)){
+        if(entero <= 1000) suma = suma + entero;
+      } 
       subCadena = "";
     }
   }
