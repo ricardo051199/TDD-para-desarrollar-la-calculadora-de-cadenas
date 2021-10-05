1204d7fa2598f32486f35d11624d35ef200b8102
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-10-05 04:08:56 -0400

    calcular suma de una cadena con un delimitador de cualquier longitud

--- src/suma-cadena.test.js
+++ src/suma-cadena.test.js
@@ -11,21 +11,33 @@ describe("sumar cadena ", () => {
   it("calcular suma de una cadena con un delimitador por defecto", () => {
     expect(calcularCadena("2, 1001", "")).toEqual(2);
   });
+  it("calcular suma de una cadena con un delimitador de cualquier longitud ", () => {
+    expect(calcularCadena("1***2***3", "***")).toEqual(6);
+  });
 });
 
 function calcularCadena(cadena, delimitador) {
-  let subCadena = "", suma = 0, baseNumerico = 10;
-  let entero;
-  for(var i = 0; i <= cadena.length; i++){
-    if(parseInt(cadena[i], baseNumerico) || cadena[i] == 0){
-      subCadena = subCadena + cadena[i];
-    }else{
-      entero = parseInt(subCadena, baseNumerico);
-      if(!delimitador || ((cadena[i] == delimitador || !cadena[i]) && entero)){
+  let suma = 0, baseNumerico = 10;
+  if(delimitador){
+    cadena = cadena.split(delimitador)
+    for(var i = 0; i < cadena.length; i++){
+      suma = suma + parseInt(cadena[i], baseNumerico);
+    }
+  } 
+  
+  if(!delimitador) {
+    let subCadena = "";
+    let entero;
+    for(var i = 0; i <= cadena.length; i++){
+      if(parseInt(cadena[i], baseNumerico) || cadena[i] == 0){
+        subCadena = subCadena + cadena[i];
+      }else{
+        entero = parseInt(subCadena, baseNumerico);
         if(entero <= 1000) suma = suma + entero;
-      } 
-      subCadena = "";
+        subCadena = "";
+      }
     }
   }
+  
   return suma;
 }
