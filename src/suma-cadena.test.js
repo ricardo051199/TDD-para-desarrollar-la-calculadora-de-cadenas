595f335146e8634076da96a2f2098db7ea22c9d4
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-10-05 03:08:58 -0400

    calcular suma de una cadena con un delimitador por defecto

--- src/suma-cadena.test.js
+++ src/suma-cadena.test.js
@@ -3,17 +3,22 @@ describe("sumar cadena ", () => {
     expect(calcularCadena("1,2,4,8", "")).toEqual(15);
   });   
   it("calcular suma para cualquier cadena", () => {
-    expect(calcularCadena("1,3,6,18", "")).toEqual(28);
+    expect(calcularCadena("1-2,3", "")).toEqual(6);
+  });
+  it("calcular suma de una cadena con un delimitador por defecto", () => {
+    expect(calcularCadena("6;7;4", ";")).toEqual(17);
   });
 });
 
 function calcularCadena(cadena, delimitador) {
   let subCadena = "", suma = 0, baseNumerico = 10;
+  let entero;
   for(var i = 0; i <= cadena.length; i++){
     if(parseInt(cadena[i], baseNumerico)){
       subCadena = subCadena + cadena[i];
     }else{
-      suma = suma + parseInt(subCadena, baseNumerico);
+      entero = parseInt(subCadena, baseNumerico)
+      if(!delimitador || ((cadena[i] == delimitador || !cadena[i]) && entero)) suma = suma + entero;
       subCadena = "";
     }
   }
