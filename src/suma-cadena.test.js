fa414425c8687409f730ec02ea04810bbc5e2a61
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-10-05 02:37:32 -0400

    calcular suma para cualquier cadena

--- src/suma-cadena.test.js
+++ src/suma-cadena.test.js
@@ -1,9 +1,21 @@
 describe("sumar cadena ", () => {
   it("calcular suma para una cadena por defecto", () => {
     expect(calcularCadena("1,2,4,8", "")).toEqual(15);
+  });   
+  it("calcular suma para cualquier cadena", () => {
+    expect(calcularCadena("1,3,6,18", "")).toEqual(28);
   });
 });
 
 function calcularCadena(cadena, delimitador) {
-  return 1 + 2 + 4 + 8;
+  let subCadena = "", suma = 0, baseNumerico = 10;
+  for(var i = 0; i <= cadena.length; i++){
+    if(parseInt(cadena[i], baseNumerico)){
+      subCadena = subCadena + cadena[i];
+    }else{
+      suma = suma + parseInt(subCadena, baseNumerico);
+      subCadena = "";
+    }
+  }
+  return suma;
 }
